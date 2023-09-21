e58b2d2e11143b884e79e493de61a6804921107c
Anna <anna.dvigun@gmail.com>
2023-09-21 13:16:56 +0300

    first commit

---
+++ test/specs/login.test.js
@@ -0,0 +1,30 @@
+//Test 1: Perform Login
+
+describe('Login saucedemo', () => {
+    it('should login using "standard_user"', async () => {
+        await browser.url('https://www.saucedemo.com/');
+
+        const usernameField = $('#user-name');
+        const passwordField = $('#password');
+        const loginButton = $('#login-button');
+
+        await usernameField.setValue('standard_user');
+        await passwordField.setValue('secret_sauce');
+
+        await loginButton.click();
+
+        const productsTitle = $('Products');
+        expect(productsTitle).toBeDisplayed();
+
+        const cartIcon = $('.shopping_cart_container');
+        expect(cartIcon).toBeDisplayed();
+
+        const products = await $$('.inventory_item');
+        await browser.waitUntil(() => products.length >= 2, {
+        timeout: 5000,
+        timeoutMsg: 'Expected at least 2 products to be present',
+    });
+
+    expect(products.length).toBeGreaterThanOrEqual(2);
+  });
+});
---
+++ test/specs/not_async.login.js
@@ -0,0 +1,25 @@
+//Test 1 not async: Perform Login
+
+describe('Login saucedemo', () => {
+    it('should login using "standard_user"', () => {
+        browser.url('https://www.saucedemo.com/');
+
+        const usernameField = $('#username');
+        const passwordField = $('#password');
+        const loginButton = $('login-button');
+
+        usernameField.setValue('standard_user');
+        passwordField.setValue('secret_sauce');
+
+        loginButton.click();
+
+        const productsTitle = $('Products');
+        expect(productsTitle).toBeDisplayed();
+
+        const cartIcon = $('.shopping_cart_container');
+        expect(cartIcon).toBeDisplayed();
+
+        const products = $$('.inventory_list');
+        expect(products).toBeElementsArrayOfSize({ gte: 2 });;
+    });
+});
